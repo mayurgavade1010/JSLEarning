a0a4ea82f4cbe57a8c00affab385cdf8d0ed4731
mayurgavade1010 <97667815+mayurgavade1010@users.noreply.github.com>
2023-09-25 17:30:30 +0530

    string
    string

---
+++ 03_funAssig02.js
@@ -0,0 +1,12 @@
+console.log("==========================================BANK DETAILS===========================================");
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("Bank Name:",bankName,"   Account NO.:",accountNum,"    Location:",location,"    Pincode:",pinCode);
+
+}
+bankDetails("CITI Bank",3456782345,'Pune',431202);
+console.log("-----------------------------------------------------------------------------------------");
+bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+console.log("-----------------------------------------------------------------------------------------");
+
+bankDetails("HDFC Bank",8956782345,'Pune',  631202);
+console.log("-----------------------------------------------------------------------------------------");
---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,15 @@
+console.log("==============================Step-1================================");
+console.log("My dream company is Persistent");
+console.log("=============================Step-2===================================");
+
+var myHobby1="Reading";
+var myHobby2="Watching Movies";
+var myHobby3="Playing Cricket";
+console.log("My hobbies are: ",myHobby1+", "+  myHobby2+", "+myHobby3);
+
+console.log("=============================Step-3===================================");
+
+var total=myHobby1.length+myHobby2.length+myHobby3.length;
+console.log("Sum of all characters are present in above hobbies= ",total);
+
+
