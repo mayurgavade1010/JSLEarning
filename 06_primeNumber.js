00e05a541d9d08324a95db81df85db7a1ce91e16
mayurgavade1010 <97667815+mayurgavade1010@users.noreply.github.com>
2023-10-17 14:59:20 +0530

    advanced function

--- 06_primeNumber.js
+++ 06_primeNumber.js
@@ -1,7 +1,7 @@
 
 function primeNumber(num){
     let count=0;
-    for (let index = 1; index <= num/2; index++) {
+    for (let index = 1; index <= num; index++) {
        if(num%index==0)
        {
         count+=1;
@@ -20,3 +20,14 @@ function primeNumber(num){
 console.log(`11 is a prime number:${primeNumber(11)}`);
 console.log(`16 is prime number:${primeNumber(16)}`);
 console.log(`19 is prime number:${primeNumber(19)}`);
+// //const num = 7;
+// function isPrimeNumber(num){
+//     for (let index = 2; index < num; index++) {
+//         if (num%index==0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// const result = isPrimeNumber(11);
+// console.log(`Is Number Prime: ${result}`);
---
+++ new.js
@@ -0,0 +1,5 @@
+const arr=[10,21,30,43,50,60];
+for (const  index in arr) {
+    const element=arr[index];
+    console.log(`Index: ${index}, value: ${element}`);
+}
