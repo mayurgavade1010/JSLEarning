027389136b44ec1fb2fd445905ca8c7c8d5cb1d8
mayurgavade1010 <97667815+mayurgavade1010@users.noreply.github.com>
2023-10-23 13:23:57 +0530

    Update 27_reduce.js

--- 27_reduce.js
+++ 27_reduce.js
@@ -1,7 +1,7 @@
 const array =  [2, 5, 3, 2, 7, 11];
-const sum = array.reduce( (runningTotal, value)=>{
-    return runningTotal + value;
-}, 0);
+const sum = array.reduce( (runningTotal,value)=>{
+    return runningTotal+value;
+},0);
 console.log(sum);
 
 
